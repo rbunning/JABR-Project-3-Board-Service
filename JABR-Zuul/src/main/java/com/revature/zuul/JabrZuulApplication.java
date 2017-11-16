d91c9e5441aa4812d1b975def78cdaf41ead1338
Regan Bunning <rtbunning@gmail.com>
2017-11-15 21:15:49 -0500

    adding Zuul

---
+++ JABR-Zuul/src/main/java/com/revature/zuul/JabrZuulApplication.java
@@ -0,0 +1,12 @@
+package com.revature.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JabrZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JabrZuulApplication.class, args);
+	}
+}
