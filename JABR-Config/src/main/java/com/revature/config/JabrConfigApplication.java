d72460ca9d8c77c343fbdccc3940ea00fdccdd82
Regan Bunning <rtbunning@gmail.com>
2017-11-16 13:36:16 -0500

    finishing touches

--- JABR-Config/src/main/java/com/revature/config/JabrConfigApplication.java
+++ JABR-Config/src/main/java/com/revature/config/JabrConfigApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
 public class JabrConfigApplication {
