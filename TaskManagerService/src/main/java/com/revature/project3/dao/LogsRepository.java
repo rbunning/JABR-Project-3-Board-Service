6e31490c2cf719fc70c8131dc86db808d6911d7a
Allen Arce <a.jarce@yahoo.com>
2017-11-29 00:20:13 -0500

    added testing and logging for Tasks

---
+++ TaskManagerService/src/main/java/com/revature/project3/dao/LogsRepository.java
@@ -0,0 +1,14 @@
+package com.revature.project3.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.revature.project3.bean.Logs;
+import com.revature.project3.bean.Task;
+
+public interface LogsRepository  extends CrudRepository<Logs, Integer>{
+
+	List<Logs> findBylogsDatedLike(String date);
+	
+}
