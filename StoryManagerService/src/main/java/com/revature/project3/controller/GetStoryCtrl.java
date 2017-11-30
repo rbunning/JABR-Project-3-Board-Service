6462ff1725853befc92abdb591bc6da1a7cb6eed
Regan Bunning <rtbunning@gmail.com>
2017-11-30 09:17:40 -0500

    bootstrap.yml are wrong but i need to get all the changes made to the other files

--- StoryManagerService/src/main/java/com/revature/project3/controller/GetStoryCtrl.java
+++ StoryManagerService/src/main/java/com/revature/project3/controller/GetStoryCtrl.java
@@ -3,16 +3,17 @@ package com.revature.project3.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.project3.beans.Story;
 import com.revature.project3.service.StoryService;
 
+@EnableResourceServer
 @RestController
 public class GetStoryCtrl {
 	
@@ -26,13 +27,13 @@ public class GetStoryCtrl {
 	
 	// Gets all the stories within a board in certain lane using key value pairs.
 	@PostMapping("/allStories")
-	public List<Story> getAllStories(Story story) {
+	public List<Story> getAllStories(@RequestBody Story story) {
 		return service.getListOfStoriesByBoardAndLaneType(story);
 	}
 	
 	// Gets all the stories within a board in certain lane with lane as part of path.
 	@PostMapping("/allStories/{lane}")
-	public List<Story> getAllStories2(Story story, @PathVariable String lane) {
+	public List<Story> getAllStories2(@RequestBody Story story, @PathVariable String lane) {
 		story.setLaneType(Integer.parseInt(lane));
 		return service.getListOfStoriesByBoardAndLaneType(story);
 	}
