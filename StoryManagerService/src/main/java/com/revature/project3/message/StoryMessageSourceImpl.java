0d1c2a632b00796d80c5595556f9a8d1cae4a9a7
William Deala <wdeala@gmail.com>
2017-12-01 09:46:14 -0500

    remove unused message package from story service

--- StoryManagerService/src/main/java/com/revature/project3/message/StoryMessageSourceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.revature.project3.message;
-
-import org.springframework.cloud.stream.annotation.EnableBinding;
-
-@EnableBinding(StoryMessageSource.class)
-public class StoryMessageSourceImpl {
-
-}
