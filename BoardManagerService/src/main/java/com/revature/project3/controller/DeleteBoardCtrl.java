30a7dadf906f2c4e48d3ee82114a13e2766a9d77
Regan Bunning <rtbunning@gmail.com>
2017-12-01 09:00:44 -0500

    fixed a couple of package names and added the ojdbc depndency to the pom file

--- BoardManagerService/src/main/java/com/revature/project3/controller/DeleteBoardCtrl.java
+++ BoardManagerService/src/main/java/com/revature/project3/controller/DeleteBoardCtrl.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.revature.project3.bean.Board;
+import com.revature.project3.beans.Board;
 import com.revature.project3.dao.BoardRepository;
 
 @RestController
